d91d3bce2ade0d6650e8809dba9cd30d33d9cc44
chenguoqing <chenguoqing@zhuojianchina.com>
2021-02-01 17:07:59 +0800

    udpate

---
+++ ask.js
@@ -0,0 +1,21 @@
+module.exports = [{
+        type: 'input',
+        name: 'projectName',
+        message: 'please input projectName',
+    },
+    {
+        type: 'confirm',
+        name: 'private',
+        message: 'ths resgistery is private?',
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'author?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'description?',
+    }
+]
